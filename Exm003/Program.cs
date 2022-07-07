29924eb3bd5ceb7e6ed2bdf690b0d09aaa717720
Dmitriy <sctoss@gmail.com>
2022-07-07 14:48:46 +0300

    Created Sum

--- Exm003/Program.cs
+++ Exm003/Program.cs
@@ -1,5 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-double numberA = 10;
-double numberB = 4;
-double result = numberA/numberB;
+int numberA = new Random().Next(1,10); // случайное число от 1 до 9
+Console.Write ("Первое случайное число: ");
+Console.WriteLine (numberA);
+int numberB = new Random().Next(1,10);
+Console.Write ("Второе случайное число: ");
+Console.WriteLine (numberB);
+int result = numberA+numberB;
+Console.Write ("Общая сумма: ");
 Console.WriteLine(result);

e6eb0b88db03cf541955f5cbb2a7f584ced6ec4a
Dmitriy <sctoss@gmail.com>
2022-07-07 14:57:18 +0300

    Changed Exm002

--- Exm002/Program.cs
+++ Exm002/Program.cs
@@ -1,5 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.Write("Введите Ваше имя: ");
 String username = Console.ReadLine();
-Console.Write("Привет, ");
-Console.Write(username);
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
+    
